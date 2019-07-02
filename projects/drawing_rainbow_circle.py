38a71f877db2293166f59c86f888b04639d535b5
Matthew Gleich <matthewgleich@gmail.com>
2019-07-02 17:45:27 -0400

    Create drawing_rainbow_circle.py

--- projects/drawing_rainbow_circle.py
+++ projects/drawing_rainbow_circle.py
@@ -1,12 +1,12 @@
 import turtle
 import random
 
-colors = ["red", "green", "blue", "yellow", "pink", "black"]
+colors = ["red", "green", "blue", "yellow", "black", "purple", "orange"]
 
 radius = 100
 while True:
     radius -= 3
-    random_num = random.randint(0, 5)
+    random_num = random.randint(0, 6)
     turtle.speed(100)
     turtle.color(colors[random_num])
     turtle.circle(radius)
